d0498c21420e4286be1c34125738d0b793360b7b
robert <robert.piotrowski92@gmail.com>
2018-07-05 20:02:05 +0200

    exception

---
+++ src/main/java/com/pokemon/exception/EmptyPokemonException.java
@@ -0,0 +1,5 @@
+package com.pokemon.exception;
+
+public class EmptyPokemonException extends Exception{
+    public EmptyPokemonException() {}
+}
---
+++ src/main/java/com/pokemon/exception/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.pokemon.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice(basePackages = "com.pokemon.rest")
+public class RestResponseEntityExceptionHandler
+        extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value
+            = { EmptyPokemonException.class })
+    public ResponseEntity<Object> handleConflict(
+            Exception ex, WebRequest request) {
+        String bodyOfResponse = "This should be application specific";
+        return handleExceptionInternal(ex, bodyOfResponse,
+                new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
